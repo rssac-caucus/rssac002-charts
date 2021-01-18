353dbc5c96a363b01fcd6dadcd759d703b0bf1b0
Andrew McConachie <andrew@depht.com>
2021-01-18 14:55:02 +0000

    Added unique-sources

---
+++ js/unique_sources.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+  var options = {
+    chart: {
+      renderTo: 'container',
+      type: 'area',
+      zoomType: 'x'
+    },
+    title: {
+        text: ''
+    },
+    subtitle: {
+        text: 'Source: RSSAC002 Data'
+    },
+    xAxis: {
+      type: 'datetime',
+      title: {
+        text: null
+      },
+    },
+    yAxis: {
+      title: {
+        text: 'Sources'
+      },
+    },
+    plotOptions: {
+      area: {
+        pointInterval: 86400000, // 1 day in ms
+        stacking: 'normal',
+        lineColor: '#666666',
+        lineWidth: 1,
+        marker: {
+          lineWidth: 1,
+          lineColor: '#666666'
+        }
+      }
+    },
+    series: [{}]
+  };
+
+  $.ajax({
+    url: "http://rssac002.depht.com/api/v1/unique-sources",
+    type: "GET",
+    dataType: "json",
+    data: {
+      letters: 'a-m',
+      start_date: '2017-01-01',
+      end_date: '2020-12-01',
+      //totals: 1,
+      //divisor: 1000
+    },
+    success: function(res){
+      var ip_version = document.getElementById('ip_version').textContent;
+      var points = [];
+      var ii = 0;
+      options.plotOptions.area.pointStart = Date.UTC('2017', '00', '01'); // Jan is zero'th month in JS
+      $.each(res, function(k_res, v_res) {
+        points[ii] = {};
+        points[ii].name = k_res;
+        points[ii].data = [];
+        $.each(v_res, function(key, val) {
+          if(val != null) {
+            points[ii].data.push(val['num-sources-ipv' + ip_version]);
+          }else{
+            points[ii].data.push(null);
+          }
+          });
+        ii += 1;
+      });
+      options.series = points;
+      options.title.text = 'Unique IPv' + ip_version + ' Sources';
+
+      var chart = new Highcharts.Chart(options);
+    }});
+});
