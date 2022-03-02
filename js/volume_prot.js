rssac-caucus/rssac002-charts

commit 7f6455e419684ccc88b14f3efabba294a530bd21

Add time_interval

js/volume_prot.js
@@ -1,6 +1,10 @@
 /* Copyright Andrew McConachie <andrew@depht.com> 2021 */
 
 $(document).ready(function() {
+  rssac002_update_chart();
+});
+
+function rssac002_update_chart(){
   var options = {
     chart: {
       renderTo: '',
@@ -31,7 +35,6 @@ $(document).ready(function() {
     },
     plotOptions: {
       area: {
-        pointInterval: 86400000, // 1 day in ms
         stacking: 'normal',
         lineColor: '#666666',
         lineWidth: 1,
@@ -47,14 +50,44 @@ $(document).ready(function() {
   // Read some values from the HTML
   var direction = document.getElementById('direction').textContent;
   var end_date = document.getElementById('end_date').textContent;
+  var time_interval = document.querySelector('input[name = "time_interval"]:checked').value;
+
+  // Determine request JSON based on time_interval
+  if(time_interval == 'day'){
+    var denominator = 1;
+    var suffix_text = ' per-day (billion)';
+    options.plotOptions.area.pointInterval =  86400000; // 1 day in ms
+    var req_data = {
+      rsi: 'a-m',
+      start_date: '2019-01-07',
+      end_date: end_date,
+    };
+  }else{
+    var denominator = 7;
+    var suffix_text = ' by-week (billion) (daily-average)';
+    options.plotOptions.area.pointInterval = 604800000; // 1 week in ms
+    var tooltip = {
+      valueDecimals: 0,
+      dateTimeLabelFormats: {
+        week:  ["Week %W, from %A, %b %e, %Y"],
+      }
+    };
+    options.tooltip = tooltip;
+    var req_data = {
+      rsi: 'a-m',
+      start_date: '2019-01-07',
+      end_date: end_date,
+      week: true,
+    };
+  }
 
   if(direction == 'received'){
     var key_str = 'queries-received';
-    var title_str = 'Queries';
+    var title_str = 'queries';
     options.yAxis.title.text = title_str;
   }else{
     var key_str = 'responses-sent';
-    var title_str = 'Responses';
+    var title_str = 'responses';
     options.yAxis.title.text = title_str;
   }
 
@@ -62,11 +95,7 @@ $(document).ready(function() {
     url: "/api/v1/traffic-volume",
     type: "GET",
     dataType: "json",
-    data: {
-      rsi: 'a-m',
-      start_date: '2017-01-01',
-      end_date: end_date,
-    },
+    data: req_data,
     success: function(res){
       var udp_v4_points = [];
       var udp_v6_points = [];
@@ -74,7 +103,7 @@ $(document).ready(function() {
       var tcp_v6_points = [];
       var ii = 0;
 
-      options.plotOptions.area.pointStart = Date.UTC('2017', '00', '01'); // Jan is zero'th month in JS
+      options.plotOptions.area.pointStart = Date.UTC('2017', '00', '07'); // Jan is zero'th month in JS
       $.each(res, function(rsi, dates) {
         udp_v4_points[ii] = {};
         udp_v4_points[ii].name = rsi;
@@ -93,7 +122,7 @@ $(document).ready(function() {
         tcp_v6_points[ii].data = [];
 
         $.each(dates, function(day, metrics) {
-          if(metrics == null){
+          if(metrics == null || metrics == 0){
             udp_v4_points[ii].data.push(null);
             tcp_v4_points[ii].data.push(null);
             udp_v6_points[ii].data.push(null);
@@ -102,39 +131,40 @@ $(document).ready(function() {
 
           $.each(metrics, function(key, value) {
             if(key == 'dns-udp-' + key_str + '-ipv4'){
-              udp_v4_points[ii].data.push(value);
+              udp_v4_points[ii].data.push(Math.round(value / denominator));
             }
             if(key == 'dns-tcp-' + key_str + '-ipv4'){
-              tcp_v4_points[ii].data.push(value);
+              tcp_v4_points[ii].data.push(Math.round(value / denominator));
             }
             if(key == 'dns-udp-' + key_str + '-ipv6'){
-              udp_v6_points[ii].data.push(value);
+              udp_v6_points[ii].data.push(Math.round(value / denominator));
             }
             if(key == 'dns-tcp-' + key_str + '-ipv6'){
-              tcp_v6_points[ii].data.push(value);
+              tcp_v6_points[ii].data.push(Math.round(value / denominator));
             }
           });
         });
         ii += 1;
       });
       options.chart.renderTo = 'container_udpv4';
-      options.title.text = 'IPv4 UDP ' + title_str + ' per-day (billion)';
+      options.title.text = 'IPv4 UDP ' + title_str + suffix_text;
       options.series = udp_v4_points;
       new Highcharts.Chart(options);
 
       options.chart.renderTo = 'container_tcpv4';
-      options.title.text = 'IPv4 TCP ' + title_str + ' per-day (billion)';
+      options.title.text = 'IPv4 TCP ' + title_str + suffix_text;
       options.series = tcp_v4_points;
       new Highcharts.Chart(options);
 
       options.chart.renderTo = 'container_udpv6';
-      options.title.text = 'IPv6 UDP ' + title_str + ' per-day (billion)';
+      options.title.text = 'IPv6 UDP ' + title_str + suffix_text;
       options.series = udp_v6_points;
       new Highcharts.Chart(options);
 
       options.chart.renderTo = 'container_tcpv6';
-      options.title.text = 'IPv6 TCP ' + title_str + ' per-day (billion)';
+      options.title.text = 'IPv6 TCP ' + title_str + suffix_text;
       options.series = tcp_v6_points;
       new Highcharts.Chart(options);
-    }});
-});
+    }
+  });
+}

js/volume_rsi.js
@@ -1,6 +1,10 @@
 /* Copyright Andrew McConachie <andrew@depht.com> 2021 */
 
 $(document).ready(function() {
+  rssac002_update_chart();
+});
+
+function rssac002_update_chart(){
   var options = {
     chart: {
       renderTo: '',
@@ -34,7 +38,6 @@ $(document).ready(function() {
     },
     plotOptions: {
       area: {
-        pointInterval: 86400000, // 1 day in ms
         stacking: 'normal',
         lineColor: '#666666',
         lineWidth: 1,
@@ -50,20 +53,48 @@ $(document).ready(function() {
   // Read some values from the HTML
   var direction = document.getElementById('direction').textContent;
   var end_date = document.getElementById('end_date').textContent;
+  var time_interval = document.querySelector('input[name = "time_interval"]:checked').value;
+
+  // Determine request JSON based on time_interval
+  if(time_interval == 'day'){
+    var denominator = 1;
+    options.plotOptions.area.pointInterval =  86400000; // 1 day in ms
+    var req_data = {
+      rsi: 'a-m',
+      start_date: '2019-01-07',
+      end_date: end_date,
+    };
+  }else{
+    var denominator = 7;
+    options.plotOptions.area.pointInterval = 604800000; // 1 week in ms
+    var tooltip = {
+      valueDecimals: 0,
+      dateTimeLabelFormats: {
+        week:  ["Week %W, from %A, %b %e, %Y"],
+      }
+    };
+    options.tooltip = tooltip;
+    var req_data = {
+      rsi: 'a-m',
+      start_date: '2019-01-07',
+      end_date: end_date,
+      week: true,
+    };
+  }
 
   if(direction == 'received'){
     var protocols = {
         'dns-udp-queries-received-ipv4': 'IPv4-UDP', 'dns-tcp-queries-received-ipv4': 'IPv4-TCP',
         'dns-udp-queries-received-ipv6': 'IPv6-UDP', 'dns-tcp-queries-received-ipv6': 'IPv6-TCP'
     };
-    var title_str = 'Queries';
+    var title_str = 'queries';
     options.yAxis.title.text = title_str;
   }else{
     var protocols = {
         'dns-udp-responses-sent-ipv4': 'IPv4-UDP', 'dns-tcp-responses-sent-ipv4': 'IPv4-TCP',
         'dns-udp-responses-sent-ipv6': 'IPv6-UDP', 'dns-tcp-responses-sent-ipv6': 'IPv6-TCP'
     };
-    var title_str = 'Responses';
+    var title_str = 'responses';
     options.yAxis.title.text = title_str;
   }
 
@@ -71,13 +102,9 @@ $(document).ready(function() {
     url: "/api/v1/traffic-volume",
     type: "GET",
     dataType: "json",
-    data: {
-      rsi: 'a-m',
-      start_date: '2017-01-01',
-      end_date: end_date,
-    },
+    data: req_data,
     success: function(res){
-      options.plotOptions.area.pointStart = Date.UTC('2017', '00', '01'); // Jan is zero'th month in JS
+      options.plotOptions.area.pointStart = Date.UTC('2017', '00', '07'); // Jan is zero'th month in JS
       var queries_series = {};
       var chart_series = {};
 
@@ -92,14 +119,14 @@ $(document).ready(function() {
         });
 
         $.each(dates, function(date, protos) {
-          if(protos == null) {
+          if(protos == null || protos == 0){
             $.each(protocols, function(key, value) {
               queries_series[rsi][key].data.push(null);
             });
           }else{
             $.each(protos, function(prot, value){
               if(prot in protocols){
-                queries_series[rsi][prot].data.push(value);
+                queries_series[rsi][prot].data.push(Math.round(value / denominator));
               }
             });
           }
@@ -110,11 +137,17 @@ $(document).ready(function() {
       });
 
       $.each(chart_series, function(rsi, protos){
+        if(time_interval == 'day'){
+          options.title.text =  rsi + '.root-servers.net ' + title_str + ' per-day (billion)';
+        }else{
+          options.title.text =  rsi + '.root-servers.net ' + title_str + ' by-week (billion) (daily-average)';
+        }
+
         options.chart.renderTo = 'container_' + rsi;
-        options.title.text =  rsi + '.root-servers.net ' + title_str + ' per-day (billion)';
         options.exporting.filename = options.title.text;
         options.series = protos;
         new Highcharts.Chart(options);
       });
-    }});
-});
+    }
+  });
+}

html_templates/volume_prot_queries
@@ -2,6 +2,19 @@
 <div hidden id="end_date">@END_DATE@</div>
 <div hidden id="direction">received</div>
 
+<div class="wrapper">
+<div class="left-inline">
+<div class="radios">
+<b>Time Interval</b><br/>
+<input type="radio" id="int_day" name="time_interval" value="day" onClick="rssac002_update_chart()">
+<label for="int_day">Day</label><br/>
+<input type="radio" id="int_week" name="time_interval" value="week" onClick="rssac002_update_chart()" checked>
+<label for="int_week">Week</label><br/>
+<br/>
+
+</div>
+</div> <!-- Closes left-inline -->
+
 <figure class="highcharts-figure">
 <div class="rchart" id="container_udpv4"></div>
 </figure>
@@ -17,4 +30,5 @@
 <figure class="highcharts-figure">
 <div class="rchart" id="container_tcpv6"></div>
 </figure>
-</div>
+</div> <!-- Closes right-inline -->
+</div> <!-- Closes main -->

html_templates/volume_prot_responses
@@ -2,6 +2,19 @@
 <div hidden id="end_date">@END_DATE@</div>
 <div hidden id="direction">sent</div>
 
+<div class="wrapper">
+<div class="left-inline">
+<div class="radios">
+<b>Time Interval</b><br/>
+<input type="radio" id="int_day" name="time_interval" value="day" onClick="rssac002_update_chart()">
+<label for="int_day">Day</label><br/>
+<input type="radio" id="int_week" name="time_interval" value="week" onClick="rssac002_update_chart()" checked>
+<label for="int_week">Week</label><br/>
+<br/>
+
+</div>
+</div> <!-- Closes left-inline -->
+
 <figure class="highcharts-figure">
 <div class="rchart" id="container_udpv4"></div>
 </figure>
@@ -17,5 +30,5 @@
 <figure class="highcharts-figure">
 <div class="rchart" id="container_tcpv6"></div>
 </figure>
-
-</div>
+</div> <!-- Closes right-inline -->
+</div> <!-- Closes main -->

html_templates/volume_rsi_queries
@@ -2,6 +2,19 @@
 <div hidden id="end_date">@END_DATE@</div>
 <div hidden id="direction">received</div>
 
+<div class="wrapper">
+<div class="left-inline">
+<div class="radios">
+<b>Time Interval</b><br/>
+<input type="radio" id="int_day" name="time_interval" value="day" onClick="rssac002_update_chart()">
+<label for="int_day">Day</label><br/>
+<input type="radio" id="int_week" name="time_interval" value="week" onClick="rssac002_update_chart()" checked>
+<label for="int_week">Week</label><br/>
+<br/>
+
+</div>
+</div> <!-- Closes left-inline -->
+
 <figure class="highcharts-figure">
 <div class="rchart" id="container_a"></div>
 </figure>
@@ -53,5 +66,5 @@
 <figure class="highcharts-figure">
 <div class="rchart" id="container_m"></div>
 </figure>
-
-</div>
+</div> <!-- Closes right-inline -->
+</div> <!-- Closes main -->

html_templates/volume_rsi_responses
@@ -2,6 +2,19 @@
 <div hidden id="end_date">@END_DATE@</div>
 <div hidden id="direction">sent</div>
 
+<div class="wrapper">
+<div class="left-inline">
+<div class="radios">
+<b>Time Interval</b><br/>
+<input type="radio" id="int_day" name="time_interval" value="day" onClick="rssac002_update_chart()">
+<label for="int_day">Day</label><br/>
+<input type="radio" id="int_week" name="time_interval" value="week" onClick="rssac002_update_chart()" checked>
+<label for="int_week">Week</label><br/>
+<br/>
+
+</div>
+</div> <!-- Closes left-inline -->
+
 <figure class="highcharts-figure">
 <div class="rchart" id="container_a"></div>
 </figure>
@@ -53,5 +66,5 @@
 <figure class="highcharts-figure">
 <div class="rchart" id="container_m"></div>
 </figure>
-
-</div>
+</div> <!-- Closes right-inline -->
+</div> <!-- Closes main -->

commit 37d2b9b7e7bf45cd9ab5185ecd8c0df2e079fcc2

Fix chart start date. Bug introduced in 7f6455e419684ccc88b14f3efabba294a530bd21

js/volume_prot.js
@@ -103,7 +103,7 @@ function rssac002_update_chart(){
       var tcp_v6_points = [];
       var ii = 0;
 
-      options.plotOptions.area.pointStart = Date.UTC('2017', '00', '07'); // Jan is zero'th month in JS
+      options.plotOptions.area.pointStart = Date.UTC('2019', '00', '07'); // Jan is zero'th month in JS
       $.each(res, function(rsi, dates) {
         udp_v4_points[ii] = {};
         udp_v4_points[ii].name = rsi;

js/volume_rsi.js
@@ -104,7 +104,7 @@ function rssac002_update_chart(){
     dataType: "json",
     data: req_data,
     success: function(res){
-      options.plotOptions.area.pointStart = Date.UTC('2017', '00', '07'); // Jan is zero'th month in JS
+      options.plotOptions.area.pointStart = Date.UTC('2019', '00', '07'); // Jan is zero'th month in JS
       var queries_series = {};
       var chart_series = {};
 