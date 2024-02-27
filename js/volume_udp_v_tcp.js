1fe546d5d41f48143826838b04527d3773461e4e
Andrew McConachie <andrew@depht.com>
2024-02-27 15:35:51 +0000

    Add week toggle and sum

--- js/volume_udp_v_tcp.js
+++ js/volume_udp_v_tcp.js
@@ -1,6 +1,10 @@
-/* Copyright Andrew McConachie <andrew@depht.com> 2021 */
+/* Copyright Andrew McConachie <andrew@depht.com> 2021 2024 */
 
 $(document).ready(function() {
+  rssac002_update_chart();
+});
+
+function rssac002_update_chart(){
   var options = {
     chart: {
       renderTo: 'container',
@@ -52,59 +56,85 @@ $(document).ready(function() {
     series: [{}]
   };
 
+  // Read some values from the HTML
+  var end_date = document.getElementById('end_date').textContent;
+  var direction = document.getElementById('direction').textContent;
+  var time_interval = document.querySelector('input[name = "time_interval"]:checked').value;
+
+  // Determine request JSON based on time_interval
+  if(time_interval == 'day'){
+    var suffix_text = 'day';
+    var point_interval =  86400000; // 1 day in ms
+    var req_data = {
+      rsi: 'a-m',
+      start_date: '2017-01-02',
+      end_date: end_date,
+      sum: true,
+    };
+  }else{
+    var suffix_text = 'week';
+    var point_interval = 604800000; // 1 week in ms
+    var tooltip = {
+      dateTimeLabelFormats: {
+        week:  ["Week %W, from %A, %b %e, %Y"],
+      }
+    };
+    options.tooltip = tooltip;
+    var req_data = {
+      rsi: 'a-m',
+      start_date: '2017-01-02',
+      end_date: end_date,
+      week: true,
+      sum: true,
+    };
+  }
+
   $.ajax({
     url: "/api/v1/traffic-volume",
     type: "GET",
     dataType: "json",
-    data: {
-      rsi: 'a-m',
-      start_date: '2017-01-01',
-      end_date: document.getElementById('end_date').textContent,
-    },
+    data: req_data,
+
     success: function(res){
-      var direction = document.getElementById('direction').textContent;
       var udp_points = {};
       var tcp_points = {};
 
-      // Get totals per-day
-      $.each(res, function(rsi, dates) {
-        $.each(dates, function(day, metrics) {
-          if(!(day in udp_points)){
-            udp_points[day] = 0;
-          }
-          if(!(day in tcp_points)){
-            tcp_points[day] = 0;
-          }
+      $.each(res, function(date, metrics) {
+        if(!(date in udp_points)){
+          udp_points[date] = 0;
+        }
+        if(!(date in tcp_points)){
+          tcp_points[date] = 0;
+        }
 
-          $.each(metrics, function(key, value) {
-            if(direction == 'received'){
-              if(key == 'dns-udp-queries-received-ipv6'){
-                udp_points[day] += value;
-              }
-              if(key == 'dns-udp-queries-received-ipv4'){
-                udp_points[day] += value;
-              }
-              if(key == 'dns-tcp-queries-received-ipv4'){
-                tcp_points[day] += value;
-              }
-              if(key == 'dns-tcp-queries-received-ipv6'){
-                tcp_points[day] += value;
-              }
-            }else{
-              if(key == 'dns-udp-responses-sent-ipv6'){
-                udp_points[day] += value;
-              }
-              if(key == 'dns-udp-responses-sent-ipv4'){
-                udp_points[day] += value;
-              }
-              if(key == 'dns-tcp-responses-sent-ipv4'){
-                tcp_points[day] += value;
-              }
-              if(key == 'dns-tcp-responses-sent-ipv6'){
-                tcp_points[day] += value;
-              }
+        $.each(metrics, function(key, value) {
+          if(direction == 'received'){
+            if(key == 'dns-udp-queries-received-ipv6'){
+              udp_points[date] += value;
             }
-          });
+            if(key == 'dns-udp-queries-received-ipv4'){
+              udp_points[date] += value;
+            }
+            if(key == 'dns-tcp-queries-received-ipv4'){
+              tcp_points[date] += value;
+            }
+            if(key == 'dns-tcp-queries-received-ipv6'){
+              tcp_points[date] += value;
+            }
+          }else{
+            if(key == 'dns-udp-responses-sent-ipv6'){
+              udp_points[date] += value;
+            }
+            if(key == 'dns-udp-responses-sent-ipv4'){
+              udp_points[date] += value;
+            }
+            if(key == 'dns-tcp-responses-sent-ipv4'){
+              tcp_points[date] += value;
+            }
+            if(key == 'dns-tcp-responses-sent-ipv6'){
+              tcp_points[date] += value;
+            }
+          }
         });
       });
 
@@ -117,11 +147,11 @@ $(document).ready(function() {
       points[1].data = Object.values(tcp_points);
 
       if(direction == 'received'){
-        options.title.text = 'UDP vs TCP Queries per-day';
+        options.title.text = 'UDP vs TCP Queries per-' + suffix_text;
       }else{
-        options.title.text = 'UDP vs TCP Responses per-day';
+        options.title.text = 'UDP vs TCP Responses per-' + suffix_text;
       }
       options.series = points;
       new Highcharts.Chart(options);
     }});
-});
+}
